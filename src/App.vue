eb8d7c44d9c85046196ef09609d3c0580fcf15f1
shin-chen <shin.chen@linecorp.com>
2021-01-06 19:38:49 +0900

    Top bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <el-container>
+   <el-header>
+    <TopBar/>
+   </el-header>
+   <el-main><HelloWorld msg="Welcome to Your Vue.js App"/></el-main>
+  </el-container>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
+import Vue from 'vue'
+import { Button, Select } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+import Element from 'element-ui'
+import './element-variables.scss'
+
+
+locale.use(lang)
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+Vue.use(Element)
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    TopBar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
